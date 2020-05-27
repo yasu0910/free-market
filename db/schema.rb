1fdee69c4bac6d1aa77187d60b53f43813663e8b
Makiti610 <kuruppooo610@gmail.com>
2020-05-27 20:18:53 +0900

    update
    初期セーブ

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2020_05_25_025313) do
+ActiveRecord::Schema.define(version: 2020_05_26_003457) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
