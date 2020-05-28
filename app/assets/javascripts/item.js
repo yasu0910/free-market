c20b2d9fe1b2994fde81006ada8c93947a6ccb16
hanaji-naji <hanaji.halloween@gmail.com>
2020-05-28 21:14:37 +0900

    add_item_edit_function
    非同期での商品編集機能の実装
    登録済み商品は表示される
    画像一枚ごとに変更可
    deviseは未実装
    カテゴリーは未実装
    ビューファイル未修正

--- app/assets/javascripts/item.js
+++ app/assets/javascripts/item.js
@@ -12,19 +12,70 @@ $(document).on('turbolinks:load', function() {
 
   // file_fieldのnameに動的なindexをつける為の配列
   let fileIndex = [1,2,3,4,5,6,7,8,9,10];
+  console.log('fileIndex;',fileIndex);
 
-  $('#image-box').on('change', '.js-file', function() {
-    // fileIndexの先頭の数字を使ってinputを作る
-    $('#image-box').append(buildFileField(fileIndex[0]));
-    fileIndex.shift();
-    // 末尾の数に1足した数を追加する
-    fileIndex.push(fileIndex[fileIndex.length - 1] + 1)
+  // 既に使われているindexを除外
+  lastIndex = $('.js-file_group:last').data('index');
+  console.log('lastIndex;',lastIndex);
+
+  // 配列の一番先頭からlastIndexまで削除
+  fileIndex.splice(0, lastIndex);
+  console.log('fileIndex after splice;',fileIndex);
+  
+
+  $('.hidden-destroy').hide();
+  // プレビュー用のimgタグを生成する関数
+  const buildImg = (index, url)=> {
+    const html = `<img data-index="${index}" src="${url}" width="100px" height="100px">`;
+    console.log('buildImg html;',html);
+    return html;
+  }
+
+  $('#image-box').on('change', '.js-file', function(e) {
+    const targetIndex = $(this).parent().data('index');
+    console.log('image-box change targetIndex;',targetIndex);
+
+    // ファイルのブラウザ上でのURLを取得する
+    const file = e.target.files[0];
+    console.log('image-box change file;',file);
+
+    const blobUrl = window.URL.createObjectURL(file);
+    console.log('image-box change blobUrl;',blobUrl);
+
+    // 該当indexを持つimgタグがあれば取得して変数imgに入れる(画像変更の処理)
+    if (img = $(`img[data-index="${targetIndex}"]`)[0]) {
+      console.log('image-box if 変更 targetIndex;',targetIndex);
+      console.log('image-box if 変更 img setAttribute before;',img);
+
+      img.setAttribute('src', blobUrl);
+      console.log('image-box if 変更 img setAttribute after;',img);
+
+      // 新規画像追加の処理
+    } else {  
+
+      console.log('image-box else 新規');
+      $('#previews').append(buildImg(targetIndex, blobUrl));
+      console.log('image-box else 新規 append(buildImg(targetIndex, blobUrl))');
+      // fileIndexの先頭の数字を使ってinputを作る
+      $('#image-box').append(buildFileField(fileIndex[0]));
+      fileIndex.shift();
+      // 末尾の数に1足した数を追加する
+      fileIndex.push(fileIndex[fileIndex.length - 1] + 1)
+    }
   });
 
   $('#image-box').on('click', '.js-remove', function() {
+    const targetIndex = $(this).parent().data('index')
+    // 該当indexを振られているチェックボックスを取得する
+    const hiddenCheck = $(`input[data-index="${targetIndex}"].hidden-destroy`);
+    // もしチェックボックスが存在すればチェックを入れる
+    if (hiddenCheck) hiddenCheck.prop('checked', true);
+    
     $(this).parent().remove();
+    $(`img[data-index="${targetIndex}"]`).remove();
+
     // 画像入力欄が0個にならないようにしておく
     if ($('.js-file').length == 0) $('#image-box').append(buildFileField(fileIndex[0]));
+    
   });
-
 });
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,9 @@
 class ItemsController < ApplicationController
+  before_action :set_item, except: [:index, :new, :create]
   def index
+    @items = Item.includes(:images).order('created_at DESC')
+    # @items = Item.all
+    # binding.pry
   end
 
   def new
@@ -16,9 +20,26 @@ class ItemsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+  end
+
   private
   def item_params
     params.require(:item).permit(:name, :content, :brand, :status, :postage, :prefecture_id, :shipping_days, :price, images_attributes: [:url, :_destroy, :id])
   end
   
+  def set_item
+    @item = Item.find(params[:id])
+  end
 end
