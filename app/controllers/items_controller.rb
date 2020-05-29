7fef4d366ac2596d612332cf0c63209394b4315a
Makiti610 <kuruppooo610@gmail.com>
2020-05-29 10:24:08 +0900

    uopdate
    カテゴリー機能実装
    また、jsの関係でviewを書き換えて独自のCSSを当ております

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,6 +5,11 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
     @item.images.build
+    
+    @category_parent_array = ["選択してください"]
+    Category.where(ancestry: nil).each do |parent|
+      @category_parent_array << parent.name
+    end
   end
 
   def create
@@ -15,10 +20,18 @@ class ItemsController < ApplicationController
       render :new
     end
   end
+  
+  def get_category_children
+    @category_children = Category.find_by(name: "#{params[:parent_name]}", ancestry: nil).children
+  end
+  
+  def get_category_grandchildren
+    @category_grandchildren = Category.find("#{params[:child_id]}").children
+  end
 
   private
   def item_params
-    params.require(:item).permit(:name, :content, :brand, :status, :postage, :prefecture_id, :shipping_days, :price, images_attributes: [:url, :_destroy, :id])
+    params.require(:item).permit(:name, :category_id, :content, :brand, :status, :postage, :prefecture_id, :shipping_days, :price, images_attributes: [:url, :_destroy, :id])
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   root 'items#index'
 
   # 仮置き
-  resources :items, except: :show
-
+  resources :items, except: :show do
+    collection do
+      get 'get_category_children', defaults: { format: 'json' }
+      get 'get_category_grandchildren', defaults: { format: 'json' }
+    end
+  end
 end
