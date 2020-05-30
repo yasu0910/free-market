a2c1f1563e6417fa6108bd8879f1f6b98253d24f
yasu0910 <yasunobu.nozaki0910@gmail.com>
2020-05-30 14:20:46 +0900

    update-devise-signup
    コンフリクト解消とビューの修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: {
     registrations: 'users/registrations',
+    sessions: 'users/sessions'   
   }
   devise_scope :user do
     get 'signup', to: 'users/registrations#signup'
