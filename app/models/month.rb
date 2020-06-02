ce53c6a4ebd8e085ecadc436fb21200f4318910e
Makiti610 <kuruppooo610@gmail.com>
2020-06-02 21:07:36 +0900

    update
    ファイル削除、微調整

--- app/models/month.rb
+++ app/models/month.rb
@@ -1,5 +1,6 @@
 class Month < ActiveHash::Base
   self.data = [
+    {id: "", name: '--'},
     {id: 1, name: '01'}, {id: 2, name: '02'}, {id: 3, name: '03'},
     {id: 4, name: '04'}, {id: 5, name: '05'}, {id: 6, name: '06'},
     {id: 7, name: '07'}, {id: 8, name: '08'}, {id: 9, name: '09'},
