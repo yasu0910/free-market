b4c2b45c90b332d69587729d1ddf41e9427af9b5
yasu0910 <yasunobu.nozaki0910@gmail.com>
2020-06-02 16:32:20 +0900

    update-sign-in&sign-up&items
    ログイン、サインアップ 時のボタンのscss変更
    サインアップ 時の都道府県をセレクターに変更
    詳細ページのマイグレーションの削除・追加

--- app/models/delivery_info.rb
+++ app/models/delivery_info.rb
@@ -1,4 +1,6 @@
 class DeliveryInfo < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
   belongs_to :user, optional: true
-  validates :first_name, :last_name, :first_name_hurigana, :last_name_hurigana, :postal_code, :state, :city, :street ,presence: true
+  validates :first_name, :last_name, :first_name_hurigana, :last_name_hurigana, :postal_code, :prefecture_id, :city, :street ,presence: true
 end
