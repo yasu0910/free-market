026f11070f75281caa763fb27953400bc7a73053
Makiti610 <kuruppooo610@gmail.com>
2020-06-03 17:41:20 +0900

    card-model-test
    カードのバリデーションテスト

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,4 +2,5 @@ class Card < ApplicationRecord
   belongs_to :user
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :month
+  validates :card_id, :customer_id, presence: true
 end
---
+++ spec/factories/cards.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :card do
+    customer_id {"customer-id-test"}
+    card_id     {"card-id-test"}
+  end
+end
