cc055e2ec40614212ad9245da71e629acaaa94b1
Makiti610 <kuruppooo610@gmail.com>
2020-06-03 23:44:42 +0900

    update
    コントローラーの条件分岐でURL直打ちでアクセス意図していないページに飛ばないように記述

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(params[:id])
+    if user_signed_in? 
+      @user = User.find(params[:id])
+      if not current_user.id == @user.id
+        redirect_to root_path
+      end
+    else
+      redirect_to root_path
+    end
   end
 end
